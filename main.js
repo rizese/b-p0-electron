8c2125ba3d6e9b35f09bf4bde3be62e814fb3b95
Ricky Franzese <thechosenrick@gmail.com>
2022-10-18 07:49:50 -0400

    Dot screen

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, 'src', 'index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
